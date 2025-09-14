1769abaecd1ec222cd14d2540adf38c99c3fb1bf
Pedro Ollero <pedroollero1998@gmail.com>
2025-09-14 20:14:41 +0200

    Add sitemap integration and update SEO metadata; create robots.txt for better indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site: "http://www.pedroollero.dev",
+  integrations: [sitemap()],
 });
